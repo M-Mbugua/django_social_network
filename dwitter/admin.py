277e4c129ce94bb0b7c45cadfd9832f6bf8a1271
M-Mbugua <mbuguamichelle9@gmail.com>
2023-06-27 21:15:25 +0000

    Project initialisation

---
+++ dwitter/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.contrib.auth.models import Group, User
+
+
+class UserAdmin(admin.ModelAdmin):
+
+    model = User
+
+    # Only display the "username" field
+
+    fields = ["username"]
+
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
